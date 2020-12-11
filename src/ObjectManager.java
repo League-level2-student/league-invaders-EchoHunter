cf87b18a9adfd4a3b856439d6a39d285054552e0
EchoHunter <48270082+EchoHunter@users.noreply.github.com>
2020-12-10 18:32:25 -0800

    :space_invader: Quick commit
    a bit I forgot to save before I uploaded last

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,4 +1,22 @@
+import java.util.ArrayList;
+import java.util.Random;
 
 public class ObjectManager {
-
+	Random random = new Random();
+	Rocketship r;
+	ObjectManager(Rocketship s){
+	r= s;
+}
+	ArrayList<Alien> aliens = new ArrayList<Alien>();
+void addAlien(){
+	aliens.add(new Alien(random.nextInt(GamePanel.WIDTH),0,50,50));
+}
+void update() {
+	for(Alien x: aliens) {
+		x.update();
+		if(x.y < GamePanel.HEIGHT ) {
+			x.isActive = false;
+		}
+	}
+}
 }
